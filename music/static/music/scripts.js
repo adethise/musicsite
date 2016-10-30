1ba1505f942c0405977f53e93d1b70cadda85a94
Arnaud Dethise <arnauddethise@gmail.com>
2016-10-31 00:02:59 +0100

    changed player to store volume and take focus on load

--- music/static/music/scripts.js
+++ music/static/music/scripts.js
@@ -1,3 +1,19 @@
+window.addEventListener("load", function() {
+	var player = document.getElementById("player");
+	player.focus();
+	if (localStorage.volume && isFinite(localStorage.volume)) {
+		player.volume = localStorage.volume;
+	} else {
+		player.volume = 1.0;
+	}
+});
+
 function startRandomSong() {
-	window.location.replace('random');
+	window.location.replace("random");
 }
+
+function storeVolume() {
+	var player = document.getElementById("player");
+	localStorage.volume = player.volume;
+}
+
