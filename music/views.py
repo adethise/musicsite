00437ca7b3e62dcd3e7d7f387e7d6c86a871046d
Arnaud Dethise <arnauddethise@gmail.com>
2016-10-30 23:03:08 +0100

    using redirection instead of serving a different view

--- music/views.py
+++ music/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, Http404
 
 from random import randrange
 
-NUM_SONGS = 601
+NUM_SONGS = 8 # this horrible hack will be removed once the model is done
 
 def index(request):
     return HttpResponseRedirect('random')
 
 def random(request):
     song_id = randrange(NUM_SONGS)
-    return song(request, song_id)
+    return redirect('song', song_id=song_id)
 
 def song(request, song_id):
     if int(song_id) not in range(NUM_SONGS):
