bfc28af23d4ae6b4dc346fa5f9199c76604fa8ca
Arnaud Dethise <arnauddethise@gmail.com>
2016-10-31 01:19:08 +0100

    song model

--- music/models.py
+++ music/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    original = models.CharField(max_length = 128)
+    name     = models.CharField(max_length = 128)
+    artist   = models.CharField(max_length = 128)
+    album    = models.CharField(max_length = 128)
+    genre    = models.CharField(max_length = 128)
+    mime     = models.CharField(max_length = 32)
+
+    def filename(self):
+        return self.pk + original[original.rfind('.'):]
+
+    def __str__(self):
+        return "%s - %s" % (artist, name)
