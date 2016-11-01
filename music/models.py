f3babb37b5797b603dc10c1fe91cf43db5930e64
Arnaud Dethise <arnauddethise@gmail.com>
2016-11-01 20:56:13 +0100

    fixed field references

--- music/models.py
+++ music/models.py
@@ -5,12 +5,12 @@ class Song(models.Model):
     original = models.CharField(max_length = 128)
     name     = models.CharField(max_length = 128)
     artist   = models.CharField(max_length = 128)
-    album    = models.CharField(max_length = 128)
     genre    = models.CharField(max_length = 128)
+    album    = models.CharField(max_length = 128)
     mime     = models.CharField(max_length = 32)
 
     def filename(self):
-        return self.pk + original[original.rfind('.'):]
+        return str(self.pk) + self.original[self.original.rfind('.'):]
 
     def __str__(self):
-        return "%s - %s" % (artist, name)
+        return "%s - %s" % (self.artist, self.name)
